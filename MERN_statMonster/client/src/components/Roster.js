06d5ebfe3f99df05fccaa2c2798d12485fc71986
jirish <jasonpbarron2@gmail.com>
2019-08-29 07:58:05 -0700

    Pulled and worked on Card component for player display

--- MERN_statMonster/client/src/components/Roster.js
+++ MERN_statMonster/client/src/components/Roster.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { Container, ListGroup, ListGroupItem, Button } from 'reactstrap';
+import { Container, ListGroup, ListGroupItem, Button, Card, CardTitle, CardSubtitle, CardBody, CardImg, CardHeader } from 'reactstrap';
 import { CSSTransition, TransitionGroup } from 'react-transition-group';
 import { connect } from 'react-redux';
 import { getPlayers, deletePlayer } from '../actions/playerActions';
@@ -21,29 +21,34 @@ class Roster extends Component {
         const { players } = this.props.player;
         return(
             <Container>
-                    <ListGroup>
+                    <ListGroup className="list-group-flush">
                         <TransitionGroup className="roster">
                             { players.map(({ _id, firstName, lastName, age, height, weight, college, jerseyNumber, primaryPosition, team, img, apiId, draftRound, draftPick, injury, draftYear }) => (
                                 <CSSTransition key={_id} timeout = {500} classNames =  "fade">
                                     <ListGroupItem>
-                                        <Button 
-                                        className="remove-btn" 
-                                        color="danger" 
-                                        size="sm" 
-                                        onClick={this.onDeleteClick.bind(this, _id)}>&times;</Button>
-                                        {firstName} {lastName} <br/>
-                                        Age: { age } <br/>
-                                        Height: { height } <br/>
-                                        Weight: { weight } <br/>
-                                        College: { college } <br/>
-                                        Jersey Number: { jerseyNumber } <br/>
-                                        Position: { primaryPosition } <br/>
-                                        Team: { team } <br/>
-                                        img: <img src={img} alt=""/> <br/>
-                                        Draft Year: { draftYear } <br/>
-                                        Round Drafted: { draftRound } <br/>
-                                        Pick Selected: { draftPick } <br/>
-                                        Current Injury: { injury } <br/>
+                                        <Card className="text-center" style={{width: '18rem'}}>
+                                            <CardHeader className="titlebar"><h3 className="PlayerName">{firstName} {lastName}</h3>
+                                            <Button 
+                                            className="remove-btn remove" 
+                                            color="danger" 
+                                            size="sm" 
+                                            onClick={this.onDeleteClick.bind(this, _id)}>&times;</Button>
+                                            </CardHeader>
+                                            <CardImg src={img} alt=""/>
+                                            <CardBody>
+                                                <CardTitle style={{fontWeight: 'bold'}}>{ primaryPosition }</CardTitle>
+                                                <CardSubtitle>{ team }</CardSubtitle>
+                                                Age: { age } <br/>
+                                                Height: { height } <br/>
+                                                Weight: { weight } <br/>
+                                                College: { college } <br/>
+                                                Jersey Number: { jerseyNumber } <br/>
+                                                Draft Year: { draftYear } <br/>
+                                                Round Drafted: { draftRound } <br/>
+                                                Pick Selected: { draftPick } <br/>
+                                                Current Injury: { injury } <br/>
+                                            </CardBody>
+                                        </Card>
                                     </ListGroupItem>
                                 </CSSTransition>
                             ))}
